dc3997753ebf79059592643b76bbd7e0a8fe41b7
prinxard <prinxard@gmail.com>
2025-06-26 10:48:59 +0100

    fixed deploy issue

---
+++ src/app/abssin/download/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+const DownloadPage = () => {
+  return (
+    <>
+    Download Abssin
+    </>
+  );
+};
+
+export default DownloadPage;
---
+++ src/app/abssin/retrieve/page.tsx
@@ -0,0 +1,9 @@
+const retrievePage = () => {
+  return (
+    <>
+    Retrieve Abssin
+    </>
+  );
+};
+
+export default retrievePage;
